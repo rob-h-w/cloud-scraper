d1cefe7afd9c40a79d3770426378d104e2538383
Rob Williamson <rob@robwilliamson.com>
2024-01-01 17:53:41 +0100

    Add a CLI Test
    This will be extended to do some simple e2e testing.

---
+++ tests/cli.rs
@@ -0,0 +1,16 @@
+use assert_cmd::Command;
+
+const BIN: &str = "cloud_scraper";
+
+#[test]
+fn run_cli_env_debug() {
+    Command::cargo_bin(BIN)
+        .unwrap()
+        .env("RUST_LOG", "debug")
+        .assert()
+        .success()
+        .stderr(predicates::str::contains("Reading config..."))
+        .stderr(predicates::str::contains("Checking config..."))
+        .stderr(predicates::str::contains("Constructing engine..."))
+        .stderr(predicates::str::contains("Starting engine"));
+}
