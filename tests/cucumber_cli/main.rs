b57d4a51ceada197ea96587424545b1028b09a79
Rob Williamson <rob@robwilliamson.com>
2024-11-03 12:00:15 +0100

    Use Fail on Skipped
    Properly so it actually fails. The WriterExt implementation isn't needed.

--- tests/cucumber_cli/main.rs
+++ tests/cucumber_cli/main.rs
@@ -1,12 +1,13 @@
 mod cli_world;
 
 use crate::cli_world::CliWorld;
-use cucumber::{World, WriterExt};
+use cucumber::World;
 
 #[tokio::main]
 async fn main() {
     let _ = CliWorld::cucumber()
+        .repeat_skipped()
+        .fail_on_skipped()
         .run_and_exit("tests/features")
-        .await
-        .fail_on_skipped();
+        .await;
 }
