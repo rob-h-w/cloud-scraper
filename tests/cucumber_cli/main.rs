a0c11fe648e1b16a3c1c46e0ab249be5b93fefe2
Rob Williamson <rob@robwilliamson.com>
2024-11-03 10:25:04 +0100

    Squish the Cucumber Features Together
    So the report counts all the features in one summary.

--- tests/cucumber_cli/main.rs
+++ tests/cucumber_cli/main.rs
@@ -5,18 +5,8 @@ use cucumber::{World, WriterExt};
 
 #[tokio::main]
 async fn main() {
-    run_all_of(vec![
-        "tests/features/config.feature",
-        "tests/features/serve.feature",
-    ])
-    .await;
-}
-
-async fn run_all_of(features: Vec<&str>) {
-    for feature in features {
-        let _ = CliWorld::cucumber()
-            .run_and_exit(feature)
-            .await
-            .fail_on_skipped();
-    }
+    let _ = CliWorld::cucumber()
+        .run_and_exit("tests/features")
+        .await
+        .fail_on_skipped();
 }
